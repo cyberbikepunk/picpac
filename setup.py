7f75d575bf51b7593c35471deafd41150639c502
cyberbikepunk <loic@cyberpunk.bike>
2015-08-23 01:51:00 +0200

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='picpac',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='Apache 2.0',
+    author='cyberbikepunk',
+    author_email='loic@cyberpunk.bike',
+    description='picpac picks and packs your pics'
+)
